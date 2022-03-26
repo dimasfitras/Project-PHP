524afa9f880d9f9c982efeec3adaf8a9bc87dbc4
Dimas Fitra Setiawan <100121529+dimasfitras@users.noreply.github.com>
2022-03-26 09:08:03 +0700

    Upload

---
+++ footer.php
@@ -0,0 +1,28 @@
+        <footer class="main-footer">
+        <div class="pull-right hidden-xs">
+        </div>
+        <strong>Copyright &copy; 2022 <a href="#">Universitas Baturaja </a>.</strong> All rights reserved.
+      </footer>
+    </div><!-- ./wrapper -->
+
+    <!-- Bootstrap 3.3.5 -->
+    <script src="../aset/bootstrap/js/bootstrap.min.js"></script>
+    <!-- FastClick -->
+    <script src="../aset/plugins/fastclick/fastclick.min.js"></script>
+    <!-- AdminLTE App -->
+    <script src="../aset/dist/js/app.min.js"></script>
+    <!-- Sparkline -->
+    <script src="../aset/plugins/sparkline/jquery.sparkline.min.js"></script>
+    <!-- jvectormap -->
+    <script src="../aset/plugins/jvectormap/jquery-jvectormap-1.2.2.min.js"></script>
+    <script src="../aset/plugins/jvectormap/jquery-jvectormap-world-mill-en.js"></script>
+    <!-- SlimScroll 1.3.0 -->
+    <script src="../aset/plugins/slimScroll/jquery.slimscroll.min.js"></script>
+    <!-- ChartJS 1.0.1 -->
+    <script src="../aset/plugins/chartjs/Chart.min.js"></script>
+    <!-- AdminLTE dashboard demo (This is only for demo purposes) -->
+    <script src="../aset/dist/js/pages/dashboard2.js"></script>
+    <!-- AdminLTE for demo purposes -->
+    <script src="../aset/dist/js/demo.js"></script>
+  </body>
+</html>
